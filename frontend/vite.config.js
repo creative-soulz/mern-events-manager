556d98a9af746428dc76dd8bd7bbbff2b85e25b3
creative-soulz <veeramanikandanart@gmail.com>
2024-06-22 12:23:00 -0700

    configuring the build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    output: 'build',
     rollupOptions: {
       external: ['@heroicons/react'],
     },
